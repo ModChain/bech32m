98dea0892ebe66fcedfd57feae5312dd5d9cc0cf
Mark Karpeles <magicaltux@gmail.com>
2024-08-04 19:55:02 +0900

    get rid of polymod loop by using a lookup table

--- polymod.go
+++ polymod.go
@@ -2,18 +2,30 @@ package bech32m
 
 // See: https://github.com/bitcoin/bips/blob/master/bip-0350.mediawiki
 
+var polymodTable = makePolymodTable()
+
+// makePolymodTable pre-compute polymod values so we can fully get rid of the loop in polymodUpdate
+func makePolymodTable() (t [32]uint32) {
+	for top := 0; top < 32; top += 1 {
+		var chk uint32
+		for i, g := range [...]uint32{0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3} {
+			if (top>>i)&1 == 1 {
+				chk ^= g
+			} else {
+				chk ^= 0
+			}
+		}
+		t[top] = chk
+	}
+	return
+}
+
 // polymodUpdate returns the updated chk value for a given value v
 // (go should inline this function easily)
 func polymodUpdate(chk uint32, v byte) uint32 {
-	top := chk >> 25
+	top := (chk >> 25) & 0x1f
 	chk = (chk&0x1ffffff)<<5 ^ uint32(v)
-	for i, g := range [...]uint32{0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3} {
-		if (top>>i)&1 == 1 {
-			chk ^= g
-		} else {
-			chk ^= 0
-		}
-	}
+	chk ^= polymodTable[top]
 	return chk
 }
 
